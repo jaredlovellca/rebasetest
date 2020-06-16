f972862be07467ddb2277c41532040a53854e125
jaredlovellca <jared.lovell@state.ca.gov>
2020-06-16 15:38:16 +0000

    I made this new file in feature branch

---
+++ derp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# a test script which does nothing.  It requires dum.conf to run correctly, though
+
+CONFIG=./dum.conf
+
+die(){
+  echo $1
+  exit 1
+}
+
+[ -f $CONFIG ] || die "unable to find my conf file, $CONFIG"
+
